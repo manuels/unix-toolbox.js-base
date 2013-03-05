6d3b4358530be2b0aee93e54bf09e448d241b197
manuels <manuel.schoelling@gmx.de>
2013-03-05 21:12:27 +0100

    Nicer virtual fs path arguments

--- interface.js
+++ interface.js
@@ -33,7 +33,6 @@
         obj = JSON.parse(ev.data);
       }
       catch(e) {
-        console.log(ev.data);
         return;
       }
 
@@ -48,43 +47,94 @@
     };
 
 
-    this.addUrl = function(real_url, pseudo_path, pseudo_name) {
+    /*
+     * Download a url and import the file into the virtual filesystem
+     *
+     * real_url:    Url to download
+     * pseudo_path: Destination path in the virtual fs
+                    if it ends with a slash, the original file name will appended
+       mkdir:       Boolean, create pseudo_path if it does not exist (default: false)
+     *
+     * returns a Promise
+     */
+    this.addUrl = function(real_url, pseudo_path, mkdir) {
+      mkdir = mkdir || false;
+
+      var opt_filename = this._analysePath(real_url).filename;
+      var dst = this._analysePath(pseudo_path, opt_filename);
+
       var prom = new Promise();
       self.promises.push(prom);
-      self.worker.postMessage(JSON.stringify({
-        cmd:         'addUrl',
-        id:          (self.promises.length-1),
-        real_url:    real_url,
-        pseudo_path: pseudo_path,
-        pseudo_name: pseudo_name
-      }));
+
+      var payload = function() {
+        self.worker.postMessage(JSON.stringify({
+          cmd:         'addUrl',
+          id:          (self.promises.length-1),
+          real_url:    real_url,
+          pseudo_path: dst.path,
+          pseudo_name: dst.filename
+        }))
+      };
+
+      if(mkdir)
+        self.mkdir(dst.path).then(payload);
+      else
+        payload();
 
       return prom;
     },
 
 
-    self.mkdir = function(pseudo_path, pseudo_name) {
+    self._analysePath = function(path_in, opt_filename) {
+      var is_absolute = (path_in[0] === '/');
+      var is_path_only = (path_in[path_in.length-1] === '/');
+
+      var filename, path;
+      if(is_path_only) {
+        filename = opt_filename || '';
+        path = path_in;
+      }
+      else {
+        var elements = path_in.split('/');
+        filename = elements[elements.length-1];
+        path = path_in.substr(0, path_in.length-filename.length);
+      }
+      return {
+        filename: filename,
+        path:     path
+      };
+    }
+
+
+    self.mkdir = function(pseudo_path) {
+      if(pseudo_path[pseudo_path.length-1] === '/')
+        pseudo_path = pseudo_path.substr(0, pseudo_path.length-1);
+
+      var dir = self._analysePath(pseudo_path);
+
       var prom = new Promise();
       self.promises.push(prom);
       self.worker.postMessage(JSON.stringify({
         cmd:         'mkdir',
         id:          (self.promises.length-1),
-        pseudo_path: pseudo_path,
-        pseudo_name: pseudo_name
+        pseudo_path: dir.path,
+        pseudo_name: dir.filename
       }));
 
       return prom;
     },
 
 
-    self.getFile = function(pseudo_path, pseudo_name) {
+    self.getFile = function(pseudo_file) {
+      var file = self._analysePath(pseudo_file);
+
       var prom1 = new Promise();
       self.promises.push(prom1);
       self.worker.postMessage(JSON.stringify({
         cmd:         'getFile',
         id:          (self.promises.length-1),
-        pseudo_path: pseudo_path,
-        pseudo_name: pseudo_name
+        pseudo_path: file.path,
+        pseudo_name: file.filename
       }));
 
       var prom2 = new Promise();
@@ -102,7 +152,7 @@
         }
 
         if(complete) {
-          prom2.fulfil(chunks.join(''), pseudo_path, pseudo_name);
+          prom2.fulfil(chunks.join(''), file.path, file.filename);
         }
       });
 
@@ -110,15 +160,17 @@
     },
 
 
-    this.addData = function(contents, pseudo_path, pseudo_name) {
+    this.addData = function(contents, pseudo_path) {
+      var dst = self._analysePath(pseudo_path);
+
       var prom = new Promise();
       self.promises.push(prom);
       self.worker.postMessage(JSON.stringify({
         cmd:         'addData',
         id:          (self.promises.length-1),
         contents:    contents,
-        pseudo_path: pseudo_path,
-        pseudo_name: pseudo_name
+        pseudo_path: dst.path,
+        pseudo_name: dst.filename
       }));
       return prom;
     },
